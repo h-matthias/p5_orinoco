c45d3f7b56cd38b49ed89e08b57281a49bbc0a79
Hoarau Matthias <h.matthias@live.fr>
2020-12-24 11:01:49 +0400

    html et js de page produit fait

--- JS/produit.js
+++ JS/produit.js
@@ -1,13 +1,30 @@
-
+//recuperation de l'id dans l'URL
 let params = new URLSearchParams(document.location.search);
 let idCameras = params.get("id")
 console.log(idCameras);
 
+//recuperation des element html
+const title = document.getElementById("title");
+const img = document.getElementById("img");
+const description = document.getElementById("description");
+const lenses = document.getElementById("lenses-select");
+const price = document.getElementById("price");
+const order = document.getElementById("order");
+
+//recuperation des donnees backend et ajout dans le html
 let cam
 fetch("http://localhost:3000/api/cameras/" + idCameras)
     .then(res => res.json())
     .then(res => {
         cam=res;
+        title.innerHTML += cam.name;
+        description.innerHTML += cam.description;
+        price.innerHTML +="Prix : "+ cam.price + " €";
+        img.setAttribute("src", cam.imageUrl);
+        img.setAttribute("alt", cam.name);
+        for (lense of cam.lenses){
+            lenses.innerHTML += `<option value:${lense}>${lense}</option> `
+        }
         console.log(cam);
     })
     .catch(error => console.error(error))
