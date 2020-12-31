dffbbf52733121f2ea19c3d2402043618bd82c98
Hoarau Matthias <h.matthias@live.fr>
2020-12-31 23:03:00 +0400

    fonction post

--- JS/basket.js
+++ JS/basket.js
@@ -2,17 +2,19 @@
 const articleBasket = document.getElementById("articleBasket");
 const totalPrice = document.getElementById("totalPrice");
 const inputName = document.getElementById("inputName");
-const inputFirstname = document.getElementById("inputfirstname");
+const inputFirstname = document.getElementById("inputFirstname");
 const inputAddress = document.getElementById("inputAddress");
 const inputCity = document.getElementById("inputCity");
 const inputEmail = document.getElementById("inputEmail");
 
 let products =[];
+/**** creation des article dans le panier via le localStorage ****/
 function writeArticle(){
     let numberArticle = 1;
     let totalBasket = 0;
-    console.log(localStorage);
-    for (let i = 0; i<= localStorage.length; i++){
+    //console.log(localStorage);
+    for (let i = 0; i<=100 /*localStorage.length*/; i++){
+        //verifie si l'article existe dans le local storage
         if (localStorage.getItem("article"+ numberArticle)!==null){
             let value = JSON.parse(localStorage.getItem("article"+ numberArticle));
             articleBasket.innerHTML += `
@@ -34,26 +36,25 @@ function writeArticle(){
             totalBasket+= value.price;
             products.push(value.id);
         } else{
-            console.log("article "+ numberArticle + " nexiste pas")
             numberArticle++;
-            
         }
     }
     totalPrice.innerHTML = (totalBasket/100).toLocaleString("fr") + ".00 €";
-}
+};
 writeArticle();
-console.log(products.length + " produit " + products )
+////console.log(products.length + " produit " + products )
 
 const btnRemove = document.querySelectorAll("button[data-parent]");
-console.log(btnRemove);
+//console.log(btnRemove);
 
+/**** Supprime l'article au click du boutton supprime via la page panier.html *****/
 function removeItem(item){
-    console.log(item.id)
+    //console.log(item.id)
     localStorage.removeItem(item.id);
-        //articleBasket.innerHTML="";
+        //recharge la page pour actualiser le panier le le total
         document.location.reload();
-        //writeArticle();
-}
+        
+};
 
 
 
@@ -64,28 +65,56 @@ function removeItem(item){
 //recupere les donner saisie de l'acheteur
 let nameContact;
 inputName.addEventListener("input",(e)=> nameContact = e.target.value);  
-let firtnameContact;
-inputName.addEventListener("input",(e)=> firstnameContact = e.target.value);
+let firstnameContact;
+inputFirstname.addEventListener("input",(e)=> firstnameContact = e.target.value);
 let addressContact;
-inputName.addEventListener("input",(e)=> addressContact = e.target.value);
+inputAddress.addEventListener("input",(e)=> addressContact = e.target.value);
 let cityContact;
-inputName.addEventListener("input",(e)=> cityContact = e.target.value);
+inputCity.addEventListener("input",(e)=> cityContact = e.target.value);
 let emailContact;
-inputName.addEventListener("input",(e)=> emailContact = e.target.value);
+inputEmail.addEventListener("input",(e)=> emailContact = e.target.value);
 
 
 let contact;
+//cree le contact 
 function createContact() {
-    
     contact = {
-        name : nameContact,
-        firstname: firtnameContact,
+        lastName : nameContact,
+        firstName: firstnameContact,
         address : addressContact,
         city : cityContact,
         email : emailContact
     }
+};
+
+const btnOrder = document.getElementById("order");
+btnOrder.addEventListener("click", (e) => {
+    e.preventDefault();
+    sendOrder();
+});
+
+
+
+function sendOrder(){
+    createContact();
+    //console.log(contact);
+
+    fetch("http://localhost:3000/api/cameras/order", {
+        method: "POST", 
+        mode : "cors",
+        headers : {"Content-Type" : "application/json"},
+        body : JSON.stringify({contact, products})
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(error => console.log(error))
 }
 
-
+function saveResOrder(res){
+    for (let response of res){
+        localStorage.setItem("firstname", response.contact.firstName);
+        console.log(localStorage.getItem("firstname"));
+    }
+}
 
 
